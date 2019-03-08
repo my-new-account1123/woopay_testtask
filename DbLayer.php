2120c53c27ef5694516d378eadea143c64acd650
my-new-account1123 <46935202+my-new-account1123@users.noreply.github.com>
2019-03-08 15:08:11 +0600

    Update DbLayer.php

--- DbLayer.php
+++ DbLayer.php
@@ -1,112 +1,134 @@
 <?php
-session_start();
-function regenerate() {
-    $_SESSION['code'] = uniqid();
-    $_SESSION['code_time'] = time();
-}
 class DbLayer
 {
-    protected $db;
-
-    public  function __construct($tablename){
-    $this->tablename=$tablename;
-       $config = require 'config.php';
-       try {
-         $this->db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['name'] . '', $config['user'],
-            $config['password']);
-     } catch (\PDOException $e) {
-        echo $e->getMessage();
-    }
-}
+	protected $db;
+	public  function __construct(string $tablename){
+		$this->tablename=$tablename;
+		$config = require 'config.php';
+		try {
+			$this->db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['name'] . '', $config['user'],
+				$config['password']);
+		} catch (\PDOException $e) {
+			echo $e->getMessage();
+		}
+	}
    // Get all records
-    public function findAll(){
-        $sth = $this->db->prepare("SELECT * FROM ".$this->tablename."");
-        $sth->execute();
-        $result = $sth -> fetchAll();
-         if( ! $result)
-        {         
-          echo "No Records Found";
-        }
-        foreach( $result as $row ) {
-        echo '<div class="news" id="news">';    
-        echo '<p>ID новости:'.$row['id'].'</p>';
-        echo '<h1>'.$row['title'].'</h1>';
-        echo '<p>'.$row['description'].'</p>';
-        echo '<p>Дата поста:'.$row['news_date'].'</p>';
-        echo '</div>'; 
-}
-    }
+	public function findAll(){
+		try {
+			$sth = $this->db->prepare('SELECT * FROM '.$this->tablename.'');
+			$sth->execute();
+			$result = $sth -> fetchAll();
+			if (!$result) {
+				throw new Exception('NoRecordsFound');
+			}
+			print_r($result);
+		}
+		catch (\Exception $e){
+			echo $e->getMessage();
+		}
+	}
     // Get one record by id
-    public function findOneById($id){
-        $sth = $this->db->prepare("SELECT * FROM ".$this->tablename." WHERE id=:id");
-        $sth->bindParam(':id', $id);    
-        $sth->execute();
-        $result = $sth->fetch();
-        if( ! $result)
-        {         
-         echo "No Records Found";
-        }
-        echo '<div class="news" id="news">';    
-        echo '<p>ID новости:'.$result['id'].'</p>';
-        echo '<h1>'.$result['title'].'</h1>';
-        echo '<p>'.$result['description'].'</p>';
-        echo '<p>Дата поста:'.$result['news_date'].'</p>';
-        echo '</div>'; 
-    
-    }
+	public function findOneById(int $id){
+		try {
+			$sth = $this->db->prepare('SELECT * FROM '.$this->tablename.' WHERE id=:id');
+			$sth->bindParam(':id', $id);    
+			$sth->execute();
+			$result = $sth->fetch();
+			if (!$result) {
+				throw new Exception('NoRecordsFound');
+			}
+			print_r($result);
+		}
+		catch (\Exception $e){
+			echo $e->getMessage();
+		}
+	}
     // Get all records by attributes
-        public function findAllByAttributes($user_id,$status){
-        $sth = $this->db->prepare("SELECT * FROM ".$this->tablename." WHERE (user_id =:user_id) OR (status =:status)");
-        $sth->bindParam(':user_id', $user_id);
-        $sth->bindParam(':status', $status);
-        $sth->execute();
-        $result = $sth->fetchAll();
-        if( ! $result)
-        {         
-         echo "No Records Found";
-        }
-        foreach( $result as $row ) {
-        echo '<div class="news" id="news">';    
-        echo '<p>ID новости:'.$row['id'].'</p>';
-        echo '<h1>'.$row['title'].'</h1>';
-        echo '<p>'.$row['description'].'</p>';
-        echo '<p>Дата поста:'.$row['news_date'].'</p>';
-        echo '</div>'; 
-    }
-    }
+	public function findAllByAttributes(array $find_attributes){
+		try {
+			$user_id=$find_attributes['user_id'];
+			$status=$find_attributes['status_news'];
+			$sth = $this->db->prepare('SELECT * FROM '.$this->tablename.' WHERE (user_id =:user_id) OR (status =:status)');
+			$sth->bindParam(':user_id', $user_id);
+			$sth->bindParam(':status', $status);
+			$sth->execute();
+			$result = $sth->fetchAll();
+			if (!$result) {
+				throw new Exception('NoRecordsFound');
+			}
+			print_r($result);
+		}
+		catch (\Exception $e){
+			echo $e->getMessage();
+		}
+	}
     // Create new record
-     public function create($user_id,$title,$description,$status){
-        $my_date = date("Y-m-d G:i:s");
-        $sth = $this->db->prepare("INSERT INTO ".$this->tablename."(user_id , title , description, news_date , status) VALUES (:user_id , :title , :description , :news_date, :status)");
-        $sth->bindParam(':user_id',$user_id);
-        $sth->bindParam(':title', $title);
-        $sth->bindParam(':description', $description);
-        $sth->bindParam(':news_date',  $my_date);
-        $sth->bindParam(':status',  $status);
-        $sth->execute();
-         if( ! $sth)
-        {         
-           echo "New records created successfully";
-        }
-       
-    }
+	public function create(array $create_attributes){
+		try {
+			$user_id=$create_attributes['user_id'];
+			$title=$create_attributes['title'];
+			$description=$create_attributes['description'];
+			$status=$create_attributes['status'];
+			$my_date = date('Y-m-d G:i:s');
+			$sth = $this->db->prepare('INSERT INTO '.$this->tablename.'(user_id , title , description, news_date , status) VALUES (:user_id , :title , :description , :news_date, :status)');
+			$sth->bindParam(':user_id',$user_id);
+			$sth->bindParam(':title', $title);
+			$sth->bindParam(':description', $description);
+			$sth->bindParam(':news_date',  $my_date);
+			$sth->bindParam(':status',  $status);
+			$sth->execute();
+			if (!$sth) {
+				throw new Exception('false');
+			}
+			else{
+				throw new Exception('true');
+			}
+		}
+		catch (\Exception $e){
+			echo $e->getMessage();
+		}
+		
+	}
     // Delete record by id
-    public function deleteById($id){
-        $sth = $this->db->prepare("DELETE FROM ".$this->tablename." WHERE id =:id");
-        $sth->bindParam(':id', $id);
-        $sth->execute();
-        echo "delete"; 
-    }
-
+	public function deleteById(int $id){
+		try{
+			$sth = $this->db->prepare('DELETE FROM '.$this->tablename.' WHERE id =:id');
+			$sth->bindParam(':id', $id);
+			$sth->execute();
+			if (!$sth) {
+				throw new Exception('false');
+			}
+			else{
+				throw new Exception('true');
+			}
+		}
+		catch (\Exception $e){
+			echo $e->getMessage();
+		}
+		
+	}
     // update record by id
-    public function updateById($id,$title,$description,$status){
-        $sth = $this->db->prepare("UPDATE ".$this->tablename."  SET title = :title , description = :description , status = :status WHERE id =:id");
-        $sth->bindParam(':id', $id);
-        $sth->bindParam(':title', $title);
-        $sth->bindParam(':description', $description);
-        $sth->bindParam(':status', $status);
-        $sth->execute();
-         echo "update"; 
-    }
-
+	public function updateById(array $update_attributes){
+		try {
+			$id=$update_attributes['update_id'];
+			$title=$update_attributes['update_title'];
+			$description=$update_attributes['update_description'];
+			$status=$update_attributes['status'];
+			$sth = $this->db->prepare('UPDATE '.$this->tablename.'  SET title = :title , description = :description , status = :status WHERE id =:id');
+			$sth->bindParam(':id', $id);
+			$sth->bindParam(':title', $title);
+			$sth->bindParam(':description', $description);
+			$sth->bindParam(':status', $status);
+			$sth->execute();
+			if (!$sth) {
+				throw new Exception('false');
+			}
+			else{
+				throw new Exception('true');
+			}
+		}
+		catch (\Exception $e){
+			echo $e->getMessage();
+		}
+	}
 }
